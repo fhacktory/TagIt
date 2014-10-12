da6ec1ea506bb810a82db782bb82e17a8b37ce5b
Martin Delille <martin@phonations.com>
2014-10-12 05:14:39 +0200

    refactoring

--- css_loader.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request,sender,sendResponse) {
-	if(request.css) {
-		chrome.tabs.insertCSS(sender.tab.id,request.css);
-		sendResponse({state:'css loaded'});
-	}
-});
