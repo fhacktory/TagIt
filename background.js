20e10b9a70fcd202603d22b8b7057a979279e55b
Martin Delille <martin@phonations.com>
2014-10-12 09:36:31 +0200

    first netawf

--- background.js
+++ background.js
@@ -11,14 +11,13 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
 
 chrome.contextMenus.removeAll();
 chrome.contextMenus.create({id: '1',title: 'Tag it!'},function() {
-	console.log(chrome.runtime.lastError);	
+	console.log(chrome.runtime.lastError);
 });
 
 chrome.contextMenus.onClicked.addListener(function(info,tab) {
 	// coordinates are here!
 	var coords = gPos;
 });
-
 
 var comments = [
   {
@@ -46,6 +45,26 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse) {
   if(request.css) {
     chrome.tabs.insertCSS(sender.tab.id,request.css);
     sendResponse({state:'css loaded'});
+  }
+  if(request.getTags) {
+    console.log("get tags");
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
+      var url = tabs[0].url;
+      console.log(url);
+      $.post(
+        'http://tagitserver.herokuapp.com/list_tags_by_url',
+        { url:url},
+        function(data) {
+          console.log(data);
+          var tags = [];
+          data.forEach(function(obj) {
+            tags.push(obj.content);
+          });
+          console.log(tags);
+          sendResponse({tags: tags});
+        }
+      );
+    });
   }
   if(request.getComments) {
     sendResponse({comments: comments});
